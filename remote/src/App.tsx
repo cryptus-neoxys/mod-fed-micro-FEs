11e60f9df3c203e845bcfc751e5180079ee1b9ac
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-11-05 14:09:37 +0530

    ✨🔧 expose Counter

--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Counter from "./Counter";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: remote</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: remote</div>
+      <Counter />
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -44,7 +44,9 @@ module.exports = {
       name: "remote",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Counter": "./src/Counter.tsx",
+      },
       shared: {
         ...deps,
         react: {
