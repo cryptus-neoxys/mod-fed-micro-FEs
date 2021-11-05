8069c4b2940d56aab56ff36ba5c3cafbcb76c527
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-11-05 14:10:02 +0530

    🎉 MICRO-FE achieved

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Counter from "remote/Counter";
+
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: host</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- host/webpack.config.js
+++ host/webpack.config.js
@@ -43,7 +43,9 @@ module.exports = {
     new ModuleFederationPlugin({
       name: "host",
       filename: "remoteEntry.js",
-      remotes: {},
+      remotes: {
+        remote: "remote@http://localhost:8081/remoteEntry.js",
+      },
       exposes: {},
       shared: {
         ...deps,
